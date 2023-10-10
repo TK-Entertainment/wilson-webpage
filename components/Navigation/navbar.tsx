e9b5bec4322053dc87185ba21ee6e46f27a04d04
Kevin Chang <52964758+kevin0216@users.noreply.github.com>
2023-10-10 09:17:30 +0000

    Update logo again

--- components/Navigation/navbar.tsx
+++ components/Navigation/navbar.tsx
@@ -81,8 +81,7 @@ export default function Navbar() {
         <Link href="/" className="text-2xl font-bold">
           <div className="max-sm:hidden">威爾森的科普天地</div>
           <div className="sm:hidden flex flex-row">
-            <FontAwesomeIcon icon={faW} className="my-auto" />
-            <div className="border-r-2 ml-2 mr-2"></div>
+            <FontAwesomeIcon icon={faW} className="my-auto mr-2" />
             <p>Notes</p>
           </div>
         </Link>
