94edb86ff5bc53caa49885882e8870d5dcd281ce
Lint Action <lint-action@samuelmeuli.com>
2023-10-28 04:09:53 +0000

    Fix code style issues with Prettier

--- app/blog/tags/[slug]/layout.tsx
+++ app/blog/tags/[slug]/layout.tsx
@@ -1,24 +1,30 @@
 import type { Metadata } from "next";
 
-export function generateMetadata({ params }: { params: { slug?: string } }): Metadata {
-    const slug = params.slug ?? "";
-    const decodedTag = decodeURIComponent(slug);
-  
-    return {
+export function generateMetadata({
+  params,
+}: {
+  params: { slug?: string };
+}): Metadata {
+  const slug = params.slug ?? "";
+  const decodedTag = decodeURIComponent(slug);
+
+  return {
+    title: {
+      template: `Wilson's Notes | 標籤 > ${decodedTag}`,
+      default: "Wilson's Notes",
+      absolute: "Wilson's Notes",
+    },
+    description:
+      "仰望星空科普專案介紹，為你帶來不同以往的科普體驗，重拾愛好自然與科學之心，讓我們一起徜徉在浩瀚星河中吧！",
+    openGraph: {
+      images: "",
       title: {
         template: `Wilson's Notes | 標籤 > ${decodedTag}`,
-        default: "Wilson's Notes",
         absolute: "Wilson's Notes",
+        default: "Wilson's Notes",
       },
-      description: "仰望星空科普專案介紹，為你帶來不同以往的科普體驗，重拾愛好自然與科學之心，讓我們一起徜徉在浩瀚星河中吧！",
-      openGraph: {
-        images: "",
-        title: {
-          template: `Wilson's Notes | 標籤 > ${decodedTag}`,
-          absolute: "Wilson's Notes",
-          default: "Wilson's Notes",
-        },
-        description: "仰望星空科普專案介紹，為你帶來不同以往的科普體驗，重拾愛好自然與科學之心，讓我們一起徜徉在浩瀚星河中吧！",
-      },
-    }
+      description:
+        "仰望星空科普專案介紹，為你帶來不同以往的科普體驗，重拾愛好自然與科學之心，讓我們一起徜徉在浩瀚星河中吧！",
+    },
   };
+}
