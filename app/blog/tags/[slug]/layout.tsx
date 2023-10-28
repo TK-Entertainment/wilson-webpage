4cf12f300b83896ab7464c80d9df786d5771e1cb
Kevin Chang <52964758+kevin0216@users.noreply.github.com>
2023-10-28 04:15:07 +0000

    Another attempt to fix Metadata Generator

--- app/blog/tags/[slug]/layout.tsx
+++ app/blog/tags/[slug]/layout.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from "next";
+import type { ReactNode } from "react";
+import { RollButton } from "next-docs-ui/components/roll-button";
 
 export function generateMetadata({
   params,
@@ -27,4 +29,14 @@ export function generateMetadata({
         "仰望星空科普專案介紹，為你帶來不同以往的科普體驗，重拾愛好自然與科學之心，讓我們一起徜徉在浩瀚星河中吧！",
     },
   };
-}
+
+  export default function TagLayout({ children }: { children: ReactNode }) {
+    return (
+      <>
+        <div className="container">
+          <RollButton />
+          {children}
+        </div>
+      </>
+    );
+  }
