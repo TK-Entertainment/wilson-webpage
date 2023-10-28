c10a193d4935fc24fab76e72311916896a0a7150
Lint Action <lint-action@samuelmeuli.com>
2023-10-28 07:35:57 +0000

    Fix code style issues with Prettier

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -1,4 +1,4 @@
-import { structure } from 'next-docs-zeta/mdx-plugins'
+import { structure } from "next-docs-zeta/mdx-plugins";
 import { allBlogs } from "contentlayer/generated";
 import { createSearchAPI } from "next-docs-zeta/search/server";
 
@@ -8,6 +8,6 @@ export const { GET } = createSearchAPI("advanced", {
     title: blog.title,
     content: blog.body.raw,
     url: `/blog/${blog.slug}`,
-    structuredData: structure(blog.body.raw)
+    structuredData: structure(blog.body.raw),
   })),
 });
