f175ba9e5ab9d88e7d4d81be49e53f0e7b9dd746
Lint Action <lint-action@samuelmeuli.com>
2023-11-10 03:29:17 +0000

    Fix code style issues with Prettier

--- components/PostLayout/postblock.tsx
+++ components/PostLayout/postblock.tsx
@@ -13,12 +13,13 @@ export default function PostBlock({ page }: { page: Blog }) {
     >
       <div className="flex flex-none max-lg:aspect-video max-lg:justify-center rounded-2xl lg:rounded-2xl border overflow-hidden lg:w-48 max-lg:h-48 h-auto object-cover">
         {page.image != null ? (
-          <Image 
-            alt="img" 
-            src={`/img/${page.slug}/${page.image}`} 
+          <Image
+            alt="img"
+            src={`/img/${page.slug}/${page.image}`}
             className="object-cover lg:object-none overflow-hidden max-lg:h-full"
-            width={500} 
-            height={400} />
+            width={500}
+            height={400}
+          />
         ) : (
           <Image
             alt="none"
@@ -31,17 +32,16 @@ export default function PostBlock({ page }: { page: Blog }) {
       </div>
       <div className="flex max-lg:p-3 flex-col ml-4 w-fit">
         {page.outside ? (
-            <div className="flex flex-row opacity-60">
-              <FontAwesomeIcon
-                icon={faUpRightFromSquare}
-                className="text-sm my-auto mr-2"
-              />
-              <p className="text-sm mr-1">外部文章</p>
-            </div>
-          ) : (
-            <>
-            </>
-          )}
+          <div className="flex flex-row opacity-60">
+            <FontAwesomeIcon
+              icon={faUpRightFromSquare}
+              className="text-sm my-auto mr-2"
+            />
+            <p className="text-sm mr-1">外部文章</p>
+          </div>
+        ) : (
+          <></>
+        )}
         <div className="flex flex-wrap">
           {page.tags?.map((tag, index) => (
             <div key={`${index}`}>
@@ -74,7 +74,14 @@ export default function PostBlock({ page }: { page: Blog }) {
           </div>
           <div className="flex flex-row content-center mt-1">
             <FontAwesomeIcon icon={faUser} className="text-base mr-2 my-auto" />
-            <p className="text-sm my-auto">{page.authors?.map((author, index) => (<span key={index}>{index != 0 ? ", " : ""}{author}</span>))}</p>
+            <p className="text-sm my-auto">
+              {page.authors?.map((author, index) => (
+                <span key={index}>
+                  {index != 0 ? ", " : ""}
+                  {author}
+                </span>
+              ))}
+            </p>
           </div>
         </div>
       </div>
