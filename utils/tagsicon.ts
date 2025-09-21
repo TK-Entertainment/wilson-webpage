86c3022b693055b2a2017e37d799e53a7d8a674d
Wilson YTC <147528651+Wilsonsonsonsonson@users.noreply.github.com>
2025-09-22 00:27:13 +0800

    Update tagsicon.ts

--- utils/tagsicon.ts
+++ utils/tagsicon.ts
@@ -16,6 +16,7 @@ import {
   faAtom,
   faFlaskVial,
   IconDefinition,
+  fa-earth-oceania,
 } from "@fortawesome/free-solid-svg-icons";
 
 const iconList = new Map<string, IconDefinition | null>();
@@ -23,6 +24,7 @@ const iconList = new Map<string, IconDefinition | null>();
 iconList.set("Young Want星空", faStar);
 iconList.set("天文板", faUserAstronaut);
 iconList.set("大氣板", faCloudShowersWater);
+iconList.set("古氣候板", fa-earth-oceania);
 iconList.set("海洋板", faWater);
 iconList.set("地質板", faHillRockslide);
 iconList.set("地球物理板", faEarthAmericas);
