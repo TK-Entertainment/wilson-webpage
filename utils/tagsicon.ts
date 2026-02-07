5c05c24298ef533f73256d9d6307b02b92f8e8ec
Kevin Chang <kevin0216@kevin0216.us>
2026-02-07 13:12:33 +0800

    fix: fixing syntax error for icon definition

--- utils/tagsicon.ts
+++ utils/tagsicon.ts
@@ -16,7 +16,7 @@ import {
   faAtom,
   faFlaskVial,
   IconDefinition,
-  fa-earth-oceania,
+  faEarthOceania,
 } from "@fortawesome/free-solid-svg-icons";
 
 const iconList = new Map<string, IconDefinition | null>();
@@ -24,7 +24,7 @@ const iconList = new Map<string, IconDefinition | null>();
 iconList.set("Young Want星空", faStar);
 iconList.set("天文板", faUserAstronaut);
 iconList.set("大氣板", faCloudShowersWater);
-iconList.set("古氣候板", fa-earth-oceania);
+iconList.set("古氣候板", faEarthOceania);
 iconList.set("海洋板", faWater);
 iconList.set("地質板", faHillRockslide);
 iconList.set("地球物理板", faEarthAmericas);
